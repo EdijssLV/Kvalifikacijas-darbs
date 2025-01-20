ad670022d2936f4f8547182af9a15cc231dad2ca
Edijs <105701308+EdijssLV@users.noreply.github.com>
2025-01-21 00:50:08 +0200

    Update alk.py

--- alk.py
+++ alk.py
@@ -3,6 +3,7 @@ from bs4 import BeautifulSoup as bs
 from decimal import Decimal, ROUND_HALF_UP
 import re
 import sqlite3
+import stores
 
 def fetch_page_content(url):
     r = requests.get(url).text
@@ -10,14 +11,7 @@ def fetch_page_content(url):
     return soup
 
 def scrape_alkoutlet():
-    shop_urls = [
-        "https://alkoutlet.lv/alus-sidri-kokteili.html?p=1&product_list_limit=36",
-        "https://alkoutlet.lv/bezalkoholiskie.html?p=1&product_list_limit=36",
-        "https://alkoutlet.lv/stiprie.html?p=1&product_list_limit=36",
-        "https://alkoutlet.lv/vins-un-vina-dzerieni.html?p=1&product_list_limit=36",
-    ]
-
-    for url in shop_urls:
+    for url in stores.AlkOutlet_urls:
         current_page = url
 
         while current_page:
@@ -60,16 +54,7 @@ def scrape_alkoutlet():
                 else:
                     cenaL = None
                 try:
-                    words_to_remove = [
-                        "Alus", "k.", "kokteilis", "Alk.", "Dzēriens", "dz.", "dzē.", "dzēr.",
-                        "kokt.", "kokteil.", "kokteilis", "B.", "v.", "Baltv.", "Baltvīns",
-                        "Bezalk.", "Brendijs", "Dabīgais", "minerālūd.", "Degv.", "Dzirkst.",
-                        "vīns", "dzēriens", "Džins", "Enerģijas", "Gāzēts", "Karstvīns", "alk.",
-                        "Konjaks", "Liķieris", "Minerālūdens", "Rums", "Rozā", "S.", "Arom.",
-                        "Sark.", "Sarkanv.", "Sidrs", "Stiprs", "Tekila", "Vermuts", "Armanjaks",
-                        "Viegli", "Viskijs", "Šampanietis", "Ūdens", "Gāz.", "Dz."]
-
-                    pattern = r"(?<!\w)(" + "|".join(re.escape(word) for word in words_to_remove) + r")(?!\w)"
+                    pattern = r"(?<!\w)(" + "|".join(re.escape(word) for word in stores.words_to_remove) + r")(?!\w)"
                     cleaned_title = re.sub(pattern, "", title)
                     title = re.sub(r"\s+", " ", cleaned_title).strip()
 
@@ -94,12 +79,7 @@ def get_next_page(soup):
     return None
 
 def scrape_rimi():
-    shop_urls = [
-        "https://www.rimi.lv/e-veikals/lv/produkti/alkoholiskie-dzerieni/c/SH-1?currentPage=1&pageSize=80",
-        "https://www.rimi.lv/e-veikals/lv/produkti/dzerieni/c/SH-5?currentPage=1&pageSize=80",
-    ]
-
-    for url in shop_urls:
+    for url in stores.rimi_urls:
         current_page = url
 
         while current_page:
@@ -154,26 +134,53 @@ def scrape_rimi():
                     cenaL = None
 
                 try:
-                    words_to_remove = [
-                        "Alus", "k.", "kokteilis", "Alk.", "Dzēriens", "dz.", "dzē.", "dzēr.",
-                        "kokt.", "kokteil.", "kokteilis", "B.", "v.", "Baltv.", "Baltvīns",
-                        "Bezalk.", "Brendijs", "Dabīgais", "minerālūd.", "Degv.", "Dzirkst.",
-                        "vīns", "dzēriens", "Džins", "Enerģijas", "Gāzēts", "Karstvīns", "alk.",
-                        "Konjaks", "Liķieris", "Minerālūdens", "Rums", "Rozā", "S.", "Arom.",
-                        "Sark.", "Sarkanv.", "Sidrs", "Stiprs", "Tekila", "Vermuts", "Armanjaks",
-                        "Viegli", "Viskijs", "Šampanietis", "Ūdens", "Gāz.", "Dz."]
-
-                    pattern = r"(?<!\w)(" + "|".join(re.escape(word) for word in words_to_remove) + r")(?!\w)"
+                    pattern = r"(?<!\w)(" + "|".join(re.escape(word) for word in stores.words_to_remove) + r")(?!\w)"
                     cleaned_title = re.sub(pattern, "", title)
                     title = re.sub(r"\s+", " ", cleaned_title).strip()
 
                 except Exception as e:
                     print(f"An error occurred: {e}")
 
+                word_mapping = {
+                    "baltvins": "Baltvīns",
+                    "sarkanvins": "Sarkanvīns",
+                    "roza-vins": "Rozā vīns",
+                    "auglu-vins": "Augļu vīns",
+                    "stiprinats-vins": "Stiprināts vīns",
+                    "sampanietis": "Šampanietis",
+                    "dzirkstosais-vins": "Dzirkstošais vīns",
+                    "alus": "Alus",
+                    "sidrs": "Sidrs",
+                    "kokteili": "Kokteiļi",
+                    "degvini": "Degvīns",
+                    "tekila": "Tekila",
+                    "dzins": "Džins",
+                    "brendijs": "Brendijs",
+                    "viskijs": "Viskijs",
+                    "kojaks": "Konjaks",
+                    "rums": "Rums",
+                    "balzams": "Balzāms",
+                    "likieris": "Liķieris",
+                    "vermuts": "Vermuts / aperitīvs",
+                    "sporta-un-funkcionālie-dzērieni": "Sporta dzērieni",
+                    "udens": "Ūdens",
+                    "limonades": "Limonādes",
+                    "sulas-un-sulu-dzerieni": "Sulas",
+                    "svaigas-sulas-smutiji": "Sulas",
+                    "sirupi": "Sīrupi / piedevas kokteiļiem",
+                    "bezalkoholiskais-alus": "Bezalkoholiskais alus",
+                    "bezalkoholiskais-sidrs": "Bezalkoholiskais sidrs",
+                    "bezalkoholiskais-vini": "Bezalkoholiskais vīni",
+                    "dzirkstosie-bezalkoholiskais-dzerieni": "Bezalkoholiskie dzērieni",
+                    "energijas-dzerieni": "Enerģijas dzērieni",}
+
+                text = current_page
+                kategorija = next((value for word, value in word_mapping.items() if word in text), None)
+
                 c.execute("""
                     INSERT INTO Kabinets (Name, Volume, Price, Store, Category, PricePerLiter, links)
                     VALUES (?, ?, ?, ?, ?, ?, ?)
-                """, (title, float(tilpums) if tilpums else None, float(cena) if cena else None, "Rimi", None, float(cenaL) if cenaL else None, link))
+                """, (title, float(tilpums) if tilpums else None, float(cena) if cena else None, "Rimi", kategorija, float(cenaL) if cenaL else None, link))
             conn.commit()
             next_page = get_next_page_rimi(soup)
             current_page = next_page
@@ -188,48 +195,7 @@ def get_next_page_rimi(soup):
     return None
 
 def scrape_SandW():
-    shop_urls = [
-            "https://www.spiritsandwine.lv/lv/absints?page=1",
-            "https://www.spiritsandwine.lv/lv/alus-alk?page=1",
-            "https://www.spiritsandwine.lv/lv/alus-bezalk?page=1",
-            "https://www.spiritsandwine.lv/lv/armanjaks?page=1",
-            "https://www.spiritsandwine.lv/lv/bag-in-box-baltivins?page=1",
-            "https://www.spiritsandwine.lv/lv/bag-in-box-sarkanvins?page=1",
-            "https://www.spiritsandwine.lv/lv/bag-in-box-sartvins?page=1",
-            "https://www.spiritsandwine.lv/lv/baltvins?page=1",
-            "https://www.spiritsandwine.lv/lv/bezalkaholiskais-vins?page=1",
-            "https://www.spiritsandwine.lv/lv/bezalkoholiskais-dzirkstosais-vins?page=1",
-            "https://www.spiritsandwine.lv/lv/bezalkoholiskais-sidrs?page=1",
-            "https://www.spiritsandwine.lv/lv/bezalkoholiskie-dzerieni?page=1",
-            "https://www.spiritsandwine.lv/lv/bezalkoholiskie-kokteili?page=1",
-            "https://www.spiritsandwine.lv/lv/bezalkoholiskie-stiprie-dzerieni?page=1",
-            "https://www.spiritsandwine.lv/lv/brendijs?page=1",
-            "https://www.spiritsandwine.lv/lv/degvins?page=1",
-            "https://www.spiritsandwine.lv/lv/dzins?page=1",
-            "https://www.spiritsandwine.lv/lv/dzirkstosais-vins?page=1",
-            "https://www.spiritsandwine.lv/lv/energijas-dzerieni?page=1",
-            "https://www.spiritsandwine.lv/lv/kalvadoss?page=1",
-            "https://www.spiritsandwine.lv/lv/karstvini-karstie-dzerieni?page=1",
-            "https://www.spiritsandwine.lv/lv/kokteili?page=1",
-            "https://www.spiritsandwine.lv/lv/konjaks?page=1",
-            "https://www.spiritsandwine.lv/lv/likieris?page=1",
-            "https://www.spiritsandwine.lv/lv/mini?page=1",
-            "https://www.spiritsandwine.lv/lv/rums?page=1",
-            "https://www.spiritsandwine.lv/lv/sampanietis?page=1",
-            "https://www.spiritsandwine.lv/lv/sarkanvins?page=1",
-            "https://www.spiritsandwine.lv/lv/sartvins?page=1",
-            "https://www.spiritsandwine.lv/lv/sidrs?page=1",
-            "https://www.spiritsandwine.lv/lv/sirupi?page=1",
-            "https://www.spiritsandwine.lv/lv/stiprinats-vins?page=1",
-            "https://www.spiritsandwine.lv/lv/sula?page=1",
-            "https://www.spiritsandwine.lv/lv/tekila?page=1",
-            "https://www.spiritsandwine.lv/lv/udens?page=1",
-            "https://www.spiritsandwine.lv/lv/uzlejums?page=1",
-            "https://www.spiritsandwine.lv/lv/vermuts?page=1",
-            "https://www.spiritsandwine.lv/lv/viskijs?page=1",
-    ]
-
-    for url in shop_urls:
+    for url in stores.SandWine_urls:
         current_page = url
 
         while current_page:
@@ -263,6 +229,12 @@ def scrape_SandW():
                 else:
                     tilpums = None
 
+                kategorija = product.find("div", class_="product-details").string.strip()
+                if ',' in kategorija:
+                    kategorija = kategorija.split(',', 1)[0]
+                else:
+                    kategorija = None
+
                 try:
                     cenaL = float((cena / tilpums).quantize(Decimal("0.01"), rounding=ROUND_HALF_UP))
                 except TypeError:
@@ -274,7 +246,7 @@ def scrape_SandW():
                 c.execute("""
                     INSERT INTO Kabinets (Name, Volume, Price, Store, Category, PricePerLiter, links)
                     VALUES (?, ?, ?, ?, ?, ?, ?)
-                """, (title, float(tilpums) if tilpums else None, float(cena) if cena else None, "Spirits & Wine", None, float(cenaL) if cenaL else None, link))
+                """, (title, float(tilpums) if tilpums else None, float(cena) if cena else None, "Spirits & Wine", kategorija, float(cenaL) if cenaL else None, link))
 
             conn.commit()
 
@@ -290,14 +262,7 @@ def get_next_page_SandW(soup):
     return None
 
 def scrape_LB():
-    shop_urls = [
-            "https://www.lbveikali.lv/lv/alus-sidrs-un-kokteili?product_list_limit=96",
-            "https://www.lbveikali.lv/lv/bezalkoholiskie-dzerieni-limonades?product_list_limit=96",
-            "https://www.lbveikali.lv/lv/stiprie-dzerieni?product_list_limit=96",
-            "https://www.lbveikali.lv/lv/vins-un-dzirkstosie?product_list_limit=96",
-    ]
-
-    for url in shop_urls:
+    for url in stores.LB_urls:
         current_page = url
 
         while current_page:
@@ -339,13 +304,21 @@ def scrape_LB():
                 except TypeError:
                     cenaL = None
 
+                kategorija = soup.find("li", class_="item category72")
+                try:
+                    kategorija = kategorija.find("strong").string.strip()
+                    if "Balzams" in kategorija:
+                        kategorija = "Balzāms"
+                except AttributeError:
+                    kategorija = None
+
                 link = product.find("a", class_="product-item-link")["href"]
                 link = str(link)
 
                 c.execute("""
                     INSERT INTO Kabinets (Name, Volume, Price, Store, Category, PricePerLiter, links)
                     VALUES (?, ?, ?, ?, ?, ?, ?)
-                """, (title, float(tilpums) if tilpums else None, float(cena) if cena else None, "Latvijas Balzāms", None, float(cenaL) if cenaL else None, link))
+                """, (title, float(tilpums) if tilpums else None, float(cena) if cena else None, "Latvijas Balzāms", kategorija, float(cenaL) if cenaL else None, link))
 
             conn.commit()
 
@@ -368,9 +341,9 @@ if __name__ == "__main__":
     conn.commit()
 
     scrape_alkoutlet()
-    scrape_rimi()
-    scrape_SandW()
-    scrape_LB()
+    #scrape_rimi()
+    #scrape_SandW()
+    #scrape_LB()
 
     c.execute(f"SELECT COUNT(*) FROM kabinets")
     row_count = c.fetchone()[0]
