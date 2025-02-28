c16c5583a8924c1cea3f07c9b3a24ef0f9979686
Edijs <105701308+EdijssLV@users.noreply.github.com>
2025-02-28 21:27:02 +0200

    f

--- users/head.php
+++ users/head.php
@@ -5,6 +5,7 @@
     <link rel="icon" type="image/x-icon" href="/images/favicon.ico">
     <link rel="stylesheet" href="styles.css">
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
     <meta name="format-detection" content="telephone=no">
     <meta name="apple-mobile-web-app-capable" content="yes">
--- users/profile.php
+++ users/profile.php
@@ -4,49 +4,83 @@ if (!isset($_SESSION["user_id"])) {
     header("Location: index.php");
     exit;
 }
+$db = new SQLite3('/var/www/mysite/database/kabinets.db', SQLITE3_OPEN_READONLY);
+
+function generateCheckboxes($db, $table, $column, $inputName) {
+    $html = '';
+    try {
+        $query = "
+            SELECT DISTINCT $column FROM Kabinets WHERE $column IS NOT NULL
+            UNION
+            SELECT DISTINCT $column FROM nemainigs WHERE $column IS NOT NULL
+            ORDER BY $column ASC
+        ";
+        $results = $db->query($query);
+        
+        while ($row = $results->fetchArray(SQLITE3_ASSOC)) {
+            $value = htmlspecialchars($row[$column], ENT_QUOTES, 'UTF-8');
+            $html .= "<label><input type=\"checkbox\" name=\"$inputName\" value=\"$value\"> $value</label><br>\n";
+        }
+    } catch (Exception $e) {
+        $html .= "<p>Error generating checkboxes: " . htmlspecialchars($e->getMessage(), ENT_QUOTES, 'UTF-8') . "</p>";
+    }
+    return $html;
+}
 ?>
-<!DOCTYPE html>
-<html lang="lv">
-    <?php include 'head.php'; ?>
-    <body background="http://st.depositphotos.com/1987851/1904/i/450/depositphotos_19041043-Old-wallpaper-seamless-texture.jpg">
-        <div class="container">
-            <div class="sidebar">
-                <h3>Menu</h3>
-                <h1>Sveiks, <?php echo htmlspecialchars($_SESSION["name"]); ?>!</h1>
-                <a href="logout.php">Izrakstīties</a>
-            </div>
-            <div class="table">
-                <div id="table1">
-                    <h1>Dzērienu tabula</h1>
-                    <table>
-                        <tr>
-                            <th>Nosaukums</th>
-                            <th>Tilpums</th>
-                            <th>Cena</th>
-                            <th>Veikals</th>
-                            <th>Kategorija</th>
-                            <th>Cena/L</th>
-                        </tr>
-                        <?php
-                            $db = new SQLite3('/var/www/mysite/database/kabinets.db');
-
-                            $sql = "SELECT * FROM Kabinets ORDER BY Name ASC";
-                            $results = $db->query($sql);
-                        ?>
-
-                        <?php while ($row = $results->fetchArray(SQLITE3_ASSOC)): ?>
-                        <tr>
-                            <td><?= htmlspecialchars($row['Name']) ?></td>
-                            <td><?= $row['Volume'] ?> L</td>
-                            <td><?= $row['Price'] ?> €</td>
-                            <td><?= htmlspecialchars($row['Store']) ?></td>
-                            <td><?= htmlspecialchars($row['Category']) ?></td>
-                            <td><?= $row['PricePerLiter'] ?> €/L</td>
-                        </tr>
-                        <?php endwhile; ?>
-                    </table>
-                </div>
-            </div>
+<?php include 'head.php'; ?>
+<body background="http://st.depositphotos.com/1987851/1904/i/450/depositphotos_19041043-Old-wallpaper-seamless-texture.jpg">
+    <h1>Kabinets</h1>
+    <div id="style">
+        <div class="sidebar">
+            <h3>Menu</h3>
+            <h2>Sveiks, <?php echo htmlspecialchars($_SESSION["name"]); ?>!</h2>
+            <a href="logout.php">Izrakstīties</a>
         </div>
-    </body>
+        <div class="table-container">
+            <table id="myTable">
+                <thead>
+                    <tr>
+                        <th>Dzēriens</th>
+                        <th>Tilpums</th>
+                        <th>Cena</th>
+                        <th>Veikals</th>
+                        <th class="hidden-column">Kategorija</th>
+                        <th>Cena/L</th>
+                        <th>Darbība</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php
+                        $db = new SQLite3('/var/www/mysite/database/kabinets.db');
+
+                        if (!$db) {
+                                die("Database connection failed: " . $db->lastErrorMsg());
+                        }
+
+                        $query = "SELECT * FROM Kabinets UNION SELECT *, NULL AS links FROM nemainigs ORDER BY Name;";
+                        $results = $db->query($query);
+
+                        if ($results) {
+                            while ($row = $results->fetchArray(SQLITE3_ASSOC)) {
+                                echo "<tr>";
+                                echo "<td><a href='" . $row['links'] . "' target='_blank'>" . $row['Name'] . "</a></td>";
+                                echo "<td>" . $row['Volume'] . " L</td>";
+                                echo "<td>" . $row['Price'] . " €</td>";
+                                echo "<td>" . $row['Store'] . "</td>";
+                                echo "<td class='hidden-column'>" . $row['Category'] . "</td>";
+                                echo "<td>" . $row['PricePerLiter'] . " €/L</td>";
+                                echo "<td><i class='fa fa-thumbs-o-up' style='font-size:24px'></i></td>";
+                                echo "</tr>";
+                            }
+                        } else {
+                            echo "No data found!";
+                        }
+                       $db->close();
+                    ?>
+                </tbody>
+            </table>
+        </div>
+    </div>
+    <script src="scripts.js"></script>
+</body>
 </html>
